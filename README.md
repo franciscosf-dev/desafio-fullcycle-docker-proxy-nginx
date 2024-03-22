e9d6ec4b058655f9d1d5ed950b0d2f737652dd67
franciscosf <franciscosf@gmail.com>
2024-03-22 16:43:57 -0300

    Finalizando o desafio

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # desafio-fullcycle-docker-proxy-nginx
+
+https://hub.docker.com/repository/docker/franciscosf/nginx/general
+
+https://hub.docker.com/repository/docker/franciscosf/node/general
--- node/index.js
+++ node/index.js
@@ -10,14 +10,36 @@ const config = {
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Francisco')`
+const nomes = ["Daniel", "Gabriel", "Thiago", "Leonardo", "Pedro", "Fernando", "Marco", "Gleica", "Yudi", "Wesley", "Renan", 
+               "Francisco", "Wallace", "Mauro", "Henrique", "Luiz", "Antônio", "Lucas", "Igor", "David", "Alvaro", "José", "João", "Paulo", "Denilson"];
+const index_maximo = nomes.length - 1; 
+const index_aleatorio = between(0,index_maximo);
+const sql = `INSERT INTO people(name) values('${nomes[index_aleatorio]}')`
 connection.query(sql)
+
+var texto = '<h1>Full Cycle</h1><br><table><tr><th>Id</th><th>Name</th></tr>'
+
+connection.query('select * from people', (err, results) => {
+    results.forEach(element => {
+        texto = texto+`<tr><td>${element.id}</td><td>${element.name}</td></tr>`
+    });
+    
+    console.log(results)
+    console.log(texto)
+  })
+
 connection.end()
 
 app.get('/', (req,res)=>{
-    res.send('<h1>Full Cycle</h1>')
+    res.send(`${texto}</table>`)
 })
 
 app.listen(port, ()=> {
     console.log('Rodando na porta '+port)
 })
+
+function between(min, max) {  
+    return Math.floor(
+      Math.random() * (max - min) + min
+    )
+  }
